e995df867390c540364a3b4354fd7deb4c43b09d
Ameer <ameerapex496@gmail.com>
2023-12-15 14:14:17 +0100

    selected elements needed

--- src/script.js
+++ src/script.js
@@ -1,1 +1,8 @@
-console.log("hallo world");
+/*  NAVBAR */
+// selecting elements
+var openCloseBtn = document.getElementsByClassName("open-Close-Btn")[0];
+var barOne = document.getElementsByClassName("bar-One")[0];
+var barTwo = document.getElementsByClassName("bar-Two")[0];
+var navbarEl = document.getElementsByClassName("navbar")[0];
+// our functions
+// our event lisnters
--- src/script.ts
+++ src/script.ts
@@ -1,1 +1,11 @@
-console.log("hallo world");
+/*  NAVBAR */
+
+// selecting elements
+const openCloseBtn = document.getElementsByClassName("open-Close-Btn")[0];
+const barOne = document.getElementsByClassName("bar-One")[0];
+const barTwo = document.getElementsByClassName("bar-Two")[0];
+const navbarEl = document.getElementsByClassName("navbar")[0];
+
+// our functions
+
+// our event lisnters
