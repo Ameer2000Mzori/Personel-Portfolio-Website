0223ef97bc4958e3d25ffeace99e3105b8069b48
Ameer <ameerapex496@gmail.com>
2023-12-16 17:56:08 +0100

    fetched data

--- src/script.ts
+++ src/script.ts
@@ -18,15 +18,32 @@ openCloseBtn.addEventListener("click", openNav);
 
 /*  PROJECTS API */
 
+// useable for later on
+
 // save for later : https://api.github.com/users/ameer2000mzori/repos
 // "url": "https://api.github.com/repos/Ameer2000Mzori/Animated-Template",
+//https://raw.githubusercontent.com/Ameer2000Mzori/Todo-List/main/sample/1.png
 
 // selecting elements
 const viewMoreBtn = document.getElementsByClassName("view-More-Btn")[0];
 
+// gelobal varibales
+let countProjects = 6;
+
 // API KEY(S)
+let API_KEY = `https://api.github.com/users/ameer2000mzori/repos?per_page=${countProjects}`;
 
 // API FETCH DATA
+async function getData(API_KEY) {
+  try {
+    const resposne = await fetch(API_KEY);
+    let data = await resposne.json();
+    console.log(data);
+  } catch (error) {
+    console.log("we got an error:", error);
+  }
+}
+getData(API_KEY);
 
 // our functions
 
